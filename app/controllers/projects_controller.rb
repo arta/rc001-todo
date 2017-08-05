2b3dfebd98c0945735b399aa120913a208c79e98
Martin Fencl <martin.fencl@gmail.com>
2017-08-05 12:34:38 -0500

    show project's incomplete tasks on /projects/show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1
   def show
+    @tasks = Task.where( project_id: @project.id, complete: false )
   end
 
   # GET /projects/new
