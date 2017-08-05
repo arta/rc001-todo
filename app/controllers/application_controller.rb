88b96e661387ebf962e8d4407aff20df9557d5e0
Martin Fencl <martin.fencl@gmail.com>
2017-08-05 08:42:11 -0500

    speed up better_errors gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :better_errors_hack, if: -> { Rails.env.development? }
+
+  private
+    # 8-5-17: Speed up extremely slow better_errors & binding_of_caller gems
+    # https://github.com/charliesome/better_errors/issues/341
+    def better_errors_hack
+      request.env['puma.config'].options.user_options.delete :app
+    end
 end
