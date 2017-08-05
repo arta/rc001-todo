d047df7ebf1d1418d8d0cde9ca29b2180bb702bd
Martin Fencl <martin.fencl@gmail.com>
2017-08-05 11:09:17 -0500

    Add project reference to tasks. Migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805144708) do
+ActiveRecord::Schema.define(version: 20170805155618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170805144708) do
     t.boolean "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 20170805144708) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "projects"
 end
